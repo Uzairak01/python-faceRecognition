cea074d8d1f2b69ce36a97f1b156f5e06bb87e8c
Uzair Ahmad Khan <h.uzairak01@gmail.com>
2025-07-23 09:52:02 +0500

    completing code

--- python-faceRecognition/main.py
+++ python-faceRecognition/main.py
@@ -48,4 +48,19 @@ while True:
     rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
     #recognize face
-    
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encoding, face_encodings)
+        best_match_index = np.argmin(face_distance)
+
+        if(matches[best_match_index]):
+            name = known_face_name[best_match_index]
+
+        cv2.imshow("Attendance", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+print("face_recognition imported from:", face_recognition.__file__)
